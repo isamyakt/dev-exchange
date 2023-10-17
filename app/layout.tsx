b25f23f0a79373cb08223c7bce7343f60c406e40
isamyakt <isamyakt17@gmail.com>
2023-10-18 03:10:51 +0530

    fixed ThemeProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,7 +43,7 @@ export default function RootLayout({
             },
           }}
         >
-          {/* <ThemeProvider>{children}</ThemeProvider> */}
+          <ThemeProvider>{children}</ThemeProvider>
         </ClerkProvider>
       </body>
     </html>
