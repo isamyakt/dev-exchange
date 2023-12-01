913239af08704d4ee156778414c9ecfe16ad02e8
isamyakt <isamyakt17@gmail.com>
2023-12-01 14:40:27 +0530

    fixed errors

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -71,16 +71,16 @@ export async function deleteUser(params: DeleteUserParams) {
     // and questions, answers, comments, etc.
 
     // get user question ids
-    // const userQuestionIds = await Question.find({ author: user._id}).distinct('_id');
+    // const userQuestionIds = await Question.find({ author: user._id }).distinct("_id");
 
     // delete user questions
     await Question.deleteMany({ author: user._id });
 
     // TODO: delete user answers, comments, etc.
 
-    const deletedUser = await User.findByIdAndDelete(user._id);
+    const deleteUser = await User.findByIdAndDelete(user._id);
 
-    return deletedUser;
+    return deleteUser;
   } catch (error) {
     console.log(error);
     throw error;
