afb0a68a2154f2fd142339d887435f7c112315ad
isamyakt <isamyakt17@gmail.com>
2023-12-03 12:18:36 +0530

    Fixed Type error: Property '_id' does not exist on type 'ModifyResult<any>'

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -61,7 +61,7 @@ export async function deleteUser(params: DeleteUserParams) {
 
     const { clerkId } = params;
 
-    const user = (await User.findOneAndDelete({ clerkId })) as any;
+    const user = await User.findOneAndDelete({ clerkId });
 
     if (!user) {
       throw new Error("User not found");
