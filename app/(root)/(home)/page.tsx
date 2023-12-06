feaae23f4e93570ceda5767de6d9dc81b2cdbd10
isamyakt <isamyakt17@gmail.com>
2023-12-06 14:10:08 +0530

    connected clerk user to ask-question page

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -12,7 +12,7 @@ import { getQuestions } from "@/lib/actions/question.action";
 export default async function Home() {
   const result = await getQuestions({});
 
-  console.log(result.questions);
+  // console.log(result.questions);
 
   return (
     <>
--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,18 +1,16 @@
 import Question from "@/components/forms/Question";
 import { getUserById } from "@/lib/actions/user.action";
-// import { auth } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
 const Page = async () => {
-  // const { userId } = auth();
-
-  const userId = "CL123";
+  const { userId } = auth();
 
   if (!userId) redirect("/sign-in");
 
   const mongoUser = await getUserById({ userId });
 
-  console.log(mongoUser);
+  // console.log(mongoUser);
 
   return (
     <div>
