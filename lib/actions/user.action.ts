a0d5e45c1d1484a46fe2abaca4e2a273dbe75a3b
isamyakt <isamyakt17@gmail.com>
2023-12-15 16:33:53 +0530

    sorting the saved questions based on view, voted oldest answered and recent

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -185,11 +185,33 @@ export async function getSavedQuestions(params: GetSavedQuestionsParams) {
       ? { title: { $regex: new RegExp(searchQuery, "i") } }
       : {};
 
+    let sortOptions = {};
+
+    switch (filter) {
+      case "most_recent":
+        sortOptions = { createdAt: -1 };
+        break;
+      case "oldest":
+        sortOptions = { createdAt: 1 };
+        break;
+      case "most_voted":
+        sortOptions = { upvotes: -1 }; // 9  -> 7 -> 4 -> 1
+        break;
+      case "most_viewed":
+        sortOptions = { views: -1 };
+        break;
+      case "most_answered":
+        sortOptions = { answer: -1 };
+        break;
+      default:
+        break;
+    }
+
     const user = await User.findOne({ clerkId }).populate({
       path: "saved",
       match: query,
       options: {
-        sort: { createAt: -1 },
+        sort: sortOptions,
       },
       populate: [
         { path: "tags", model: Tag, select: "_id name " },
